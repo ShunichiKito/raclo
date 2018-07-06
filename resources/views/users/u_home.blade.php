1158a45a62190154473d30c343a81420a269130a
sho.ikuta <sho.ikuta@rakuten.com>
2018-07-06 07:25:59 +0000

    done

--- resources/views/users/u_home.blade.php
+++ resources/views/users/u_home.blade.php
@@ -6,10 +6,10 @@
             <!--<div style="position: relative;">-->
                 <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/u_home.css') }}">
                 <!--<img src="cabinet-575361_1280.png" class="center" alt="" height=500 width=500>-->
-                <link a href="stylists.s_index"><img src="hanger-29414_1280.png" class="left" alt="" height=100 width=100></link>
-                <link a href="/"><img src="star-158502_640.png" class="right" alt="" height=100 width=100></link>
+               
+                <a href="u_index"><img src="hanger-29414_1280.png" class="left" alt="" height=100 width=100"></a>
+                <a href="u_mycoordinates"><img src="star-158502_640.png" class="right" alt="" height=100 width=100"></a>
                 <!--<div style="position: absolute; top: 30px; left: 100px;">-->
-                    
         </div>
     </div>
 @endsection
