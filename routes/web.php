4553c1d96d0670f659a72117d438d776b68d422e
marienaomi.iwata <marienaomi.iwata@rakuten.com>
2018-07-10 12:21:29 +0000

    price boxs create

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,11 @@ Route::get('/u_home', function () {
 })->name('u_home');
 
 
+Route::get('/u_price', function () {
+    return view('users/u_price');
+})->name('u_price');
+
+
 // // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -39,15 +44,6 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 // // ログイン認証
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
-
-//ログイン後画面
-Route::get('/users/u_home', function () {
-    return view('users/u_home');
-});
-Route::get('/stylists/s_home', function () {
-    return view('stylists/s_home');
-});
-
 
 
 // Route::group(['middleware' => ['auth']], function () {
