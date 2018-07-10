217fa4acd2f2c59951175bef68c8c0d50be65b17
shunichi.kito <shunichi.kito@rakuten.com>
2018-07-10 09:17:03 +0000

    login adjusted

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,16 @@
 <?php
 $user = \Auth::user()->id;
-        
+     
 if (\Auth::user()->user_type == 1){ ?>
 
-            @include('users.u_home')
-                <!--// return view('users.u_home', [-->
-                <!--//     'user' => $user,-->
-                <!--// ]);-->
-            <?php } elseif(\Auth::user()->user_type == 2)  { ?>
-            @include('stylists.s_home')
-                <!--//   return view('stylists.s_home', [-->
-                <!--//     'user' => $user,-->
-                <!--// ]);-->
-          <?php  } else{ 
-                  
-                     return redirect('/');
-             }
+    @include('users.u_home')
+        
+<?php } elseif(\Auth::user()->user_type == 2)  { ?>
+    @include('stylists.s_home')
+       
+<?php  } else{ 
+    return redirect('/');
+}
              
 ?>
 <a href="/logout"> Logout </a>
