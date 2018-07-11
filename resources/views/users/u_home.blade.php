dc41e1b9de8261dd397251a5443913f194589bfa
ichiro.tanimoto <ec2-user@ip-172-31-39-134.us-east-2.compute.internal>
2018-07-11 05:35:07 +0000

    create closettab

--- resources/views/users/u_home.blade.php
+++ resources/views/users/u_home.blade.php
@@ -13,5 +13,57 @@
                 
                 <!--<div style="position: absolute; top: 30px; left: 100px;">-->
         </div>
-    </div>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+
+        <div class="tab">
+            <button class="tablinks" onclick="openCloset(event, 'Request')" id="defaultOpen">withinmyitems</button>
+            <button class="tablinks" onclick="openCloset(event, 'Coordinate')">withnewitems</button>
+        </div>
+
+        <div id="Request" class="tabcontent">
+        <h3>ここにリクエスト用画像</h3>
+
+        </div>
+
+        <div id="Coordinate" class="tabcontent">
+        <h3>ここにコーディネイト用画像</h3>
+
+        </div>
+        
+        
+        <script>
+        function openCloset(evt, cityName) {
+           var i, tabcontent, tablinks;
+           tabcontent = document.getElementsByClassName("tabcontent");
+           for (i = 0; i < tabcontent.length; i++) {
+               tabcontent[i].style.display = "none";
+           }
+           tablinks = document.getElementsByClassName("tablinks");
+           for (i = 0; i < tablinks.length; i++) {
+               tablinks[i].className = tablinks[i].className.replace(" active", "");
+           }
+           document.getElementById(cityName).style.display = "block";
+           evt.currentTarget.className += " active";
+        }
+        document.getElementById("defaultOpen").click();
+        
+        </script>
+           
+    </body>
+</html>
+</div>
 @endsection
+
+
+
+
+
+(img src="
+
+codinate item
+
+
