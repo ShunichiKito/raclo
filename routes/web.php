d3e06cf1d2a35eeff380c1a9ec717f1a9194d89b
misato.otaki <misato.otaki@rakuten.com>
2018-07-11 06:20:05 +0000

    sprice

--- routes/web.php
+++ routes/web.php
@@ -43,13 +43,21 @@ Route::get('/s_home', function () {
     return view('stylists/s_home');
 })->name('s_home');
 
-Route::get('/privacy', function () {
-    return view('users/privacy');
-})->name('privacy');
+Route::get('/u_privacy', function () {
+    return view('users/u_privacy');
+})->name('u_privacy');
+
+Route::get('/s_privacy', function () {
+    return view('stylists/s_privacy');
+})->name('s_privacy');
 
 Route::get('/u_price', function () {
     return view('users/u_price');
 })->name('u_price');
+
+Route::get('/s_price', function () {
+    return view('stylists/s_price');
+})->name('s_price');
 
 
 
