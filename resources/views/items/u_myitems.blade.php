c69295a4275b9a25ca1774fc703121750c7f60dc
sho.ikuta <sho.ikuta@rakuten.com>
2018-07-13 03:30:35 +0000

    aaa

--- resources/views/items/u_myitems.blade.php
+++ resources/views/items/u_myitems.blade.php
@@ -13,22 +13,7 @@
 
                                     <input type="checkbox" name="item" value="$item" id="item" />
                                     <p id="p1"></p>
-                                    <script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
-                                    <script>
-                                    $function() {
-                                        $('input[name="item"]').change(function() {
-                                        
-                                            // 選択したValue値を変数にうつす
-                                            var val = $('#val:checked').val();
-                                            
-                                            if (val) {
-                                                $('#p1').text('valkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkでチェック');
-                                            }else {
-                                                $('#p1').text('');
-                                            }
-                                        }
-                                    )});
-                                    </script>
+                                   
                                     
 
                                 {{ Form::checkbox('item[]',"$item->file_path", false) }}
