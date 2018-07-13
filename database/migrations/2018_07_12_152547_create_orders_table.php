cb97074d81d02e24af90df585d342fdcb3f92c9e
shunichi.kito <shunichi.kito@rakuten.com>
2018-07-13 00:31:04 +0000

    order table modified

--- database/migrations/2018_07_12_152547_create_orders_table.php
+++ database/migrations/2018_07_12_152547_create_orders_table.php
@@ -15,9 +15,16 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('user_id');
-            $table->string('stylist_id');
+            $table->integer('user_id');
+            $table->integer('stylist_id');
             $table->timestamps();
+            
+            // 外部キー設定
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('stylist_id')->references('id')->on('users')->onDelete('cascade');
+
+            // user_idとfollow_idの組み合わせの重複を許さない
+            $table->unique(['user_id', 'stylist_id']);
         });
     }
 
