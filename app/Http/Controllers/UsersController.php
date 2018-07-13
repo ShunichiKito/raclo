98058954c1f350a5b3aa324dd46c31f2d80a05e0
marienaomi.iwata <marienaomi.iwata@rakuten.com>
2018-07-13 06:30:10 +0000

    fixed route

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -73,10 +73,9 @@ class UsersController extends Controller
              }
         }    
         
-        
     }
     
-     public function update(Request $request, $id)
+    public function update(Request $request, $id)
     {
         $user = User::find($id);
         if (\Auth::id() == $user->id){
@@ -89,7 +88,7 @@ class UsersController extends Controller
                 $user->age = $request->age;
                 $user->gender = $request->gender;
                 $user->save();
-                return redirect('/u_home');
+                return redirect('/home');
          
          } elseif (\Auth::user()->user_type == 2){
                 $this->validate($request, [ 
@@ -104,13 +103,13 @@ class UsersController extends Controller
                 $user->background = $request->background;
                 $user->style = $request->style;
                 $user->save();
-                return redirect('/s_home');
+                return redirect('/home');
 
          }else{
                   
             return redirect('/');
          }
-                                    }
+        }
     }
     
      public function myregister(Request $request)
@@ -139,53 +138,5 @@ class UsersController extends Controller
         }
         return register('/u_stylist_lists');
         
-    }   
-    
+    }  
 }
-
-
-// public function edit($id)
-//     {
-        
-//          $user = User::find($id);
-//         if (\Auth::id() == $user->id)
-//         if (\Auth::user_type() == 1)
-//         {
-
-//         return view('stylists.s_edit', [
-//             'user' => $user,
-//         ]);
-//           } else {
-//              return redirect('/');
-//         }
-        
-        
-//     }
-    
-// public function update(Request $request, $id)
-//     {
-//         $this->validate($request, [ 
-//             'age' => 'required|max:191',
-//             'gender'=> 'required|max:10',
-//             'background'=> 'required|max:191',
-//             'style' => 'style|max;191',
-//         ]);
-
-
-//         $user = User::find($id);
-//          if (\Auth::id() == $user->id){
-        
-//         $user->age = $request->age;
-//         $user->gender = $request->gender;
-//         $user->background = $request->background;
-//         $user->style = $request->style;
-//         $user->save();
-//          return redirect('/s_home');
-//          }else{
-
-//              return redirect('/');
-// //         }
-// //     }
-    
-// // }
-
