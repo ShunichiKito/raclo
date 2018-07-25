ff07409ae693cbee185cf4d73a76835438a0839a
shunichi.kito <shunichi.kito@rakuten.com>
2018-07-25 03:41:53 +0000

    show function complete

--- resources/views/users/u_coord_show.blade.php
+++ resources/views/users/u_coord_show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.user_app')
-
-
+<?php
+use App\Coordinated_set;
+?>
 @section('content')
    
    
@@ -13,10 +14,55 @@
 	<div class="panel-body">
 	    coordinates or suggestion
 	    <?php 
-	    if(empty($order->item1)) {
-	    	 print $order->item1;
-	    }
+	  
+	    $coordinates=Coordinated_set::where('user_id', \Auth::user()->id)->where('stylist_id',$order->stylist_id)->where('order_id',$order->id)->get();
 	   
+	    foreach($coordinates as $key => $coordinate) { ?>
+	    	<p>coordinate<?php print $key+1;?></p>
+	        <?php if(!empty($coordinate->item1)) { ?>
+		    	<div>
+		    		<img class="co_image" src="<?php print $coordinate->item1; ?>">
+		    	</div>
+	    	<?php }
+	    	if(!empty($coordinate->item2)) { ?>
+		    	<div>
+		    		<img class="co_image" src="<?php print $coordinate->item2; ?>">
+		    	</div>
+	    	<?php }
+	    	if(!empty($coordinate->item3)) { ?>
+		    	<div>
+		    		<img class="co_image" src="<?php print $coordinate->item3; ?>">
+		    	</div>
+	    	<?php }
+	    	if(!empty($coordinate->item4)) { ?>
+		    	<div>
+		    		<img class="co_image" src="<?php print $coordinate->item4; ?>">
+		    	</div>
+	    	<?php }
+	    	if(!empty($coordinate->item5)) { ?>
+		    	<div>
+		    		<img class="co_image" src="<?php print $coordinate->item5; ?>">
+		    	</div>
+	    	<?php }
+	    	if(!empty($coordinate->item6)) { ?>
+		    	<div>
+		    		<img class="co_image" src="<?php print $coordinate->item6; ?>">
+		    	</div>
+	    	<?php }
+	    	if(!empty($coordinate->item7)) { ?>
+		    	<div>
+		    		<img class="co_image" src="<?php print $coordinate->item7; ?>">
+		    	</div>
+	    	<?php }
+	    }
+	    
+	   ?>
+	   <style>
+		   	img.co_image {
+		    width: 100px;
+		    height: 100px;
+			}
+	   </style>
 	</div>
 
 	
