9b83fe6aa15247d00c4b412ff534c501ee48123e
marienaomi.iwata <marienaomi.iwata@rakuten.com>
2018-07-25 04:51:57 +0000

    fix privacy

--- resources/views/users/u_privacy.blade.php
+++ resources/views/users/u_privacy.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-lg-12">
-            <h1>Privacy and Security</h1>
+            <h1><strong>Privacy and Security</strong></h1>
             
         <br>
         <br>
@@ -11,7 +11,7 @@
             
             
     <section>
-        <h2>Privacy Policy</h2>
+        <h2><strong>Privacy Policy</strong></h2>
         <p>Our Privacy Center provides various information on our effort to protect your privacy. We will keep updating and expanding the contents.</p>
         <p>Pandanity group aims to provide you with a unique and personalized experience when you interact with the Our Group’s services. In order to achieve this goal, the Pandanity Group provides a unified ID program called “Pandanity ID,” which includes a single log-in function that makes it easy for you to access and use applicable services provided globally by the Pandanity Group companies. You may create your own Pandanity account (your “Account”), and use all related services (together, the “Membership Service”) in accordance with this Policy and the Pandanity Membership Terms & Conditions set forth separately by Pandanity.</p>
     
@@ -22,7 +22,7 @@
     
 
     <section>
-        <h2>Our Security</h2>
+        <h2><strong>Our Security</strong></h2>
 
         <p>Our contact information is listed there and we, with our data protection officer, will respond without undue delay to you. </p>
         <p>We are committed to handing your personal information in accordance with high security standards and in compliance with applicable law. We do so in order to prevent unauthorized access or unauthorized disclosure of your information, to maintain the accuracy of such information and, when necessary, to ensure its proper destruction. When collecting or transferring your personal information, we employ data encryption technology, and restrict access only to those persons who require it to fulfil their job responsibilities. We conduct periodic reviews of our practices to ensure that our safeguards are properly implemented and remain state of the art.</p>
@@ -30,3 +30,10 @@
         </div>
     </div>
 @endsection
+
+<style>
+body {
+    background-image: url('/priceimg/4827196796_c511b4be80_o.jpg');
+}
+    
+</style>
